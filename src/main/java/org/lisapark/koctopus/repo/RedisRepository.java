3eb8ded6db6ba254e1f4a95ebb185e994c768569
alexmy <alexmy@alexmy-ThinkPad-P51>
2019-08-15 17:39:22 -0400

    rollback to separate projects and some clean up

--- src/main/java/org/lisapark/koctopus/repo/RedisRepository.java
+++ src/main/java/org/lisapark/koctopus/repo/RedisRepository.java
@@ -6,16 +6,12 @@
 package org.lisapark.koctopus.repo;
 
 import org.lisapark.koctopus.core.OctopusRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author alexmy
  */
 public class RedisRepository extends AbstractOctopusRepository implements OctopusRepository {
-
-    private static final Logger LOG = LoggerFactory.getLogger(RedisRepository.class);
 
     public RedisRepository() {
         super();
